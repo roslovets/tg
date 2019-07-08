b00b3762dea891ff8810e5faa3a3ca34b4b70f29
Pavel Roslovets <roslovets@exponenta.ru>
2019-07-08 19:26:34 +0300

    Improve styles, new instruction

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import './index.scss';
 
 var lang = navigator.language || navigator.userLanguage;
+if (lang === 'ru-RU') {
+    var TGSTR = 'Открыть в Telegram';
+    var WEBSTR = 'Открыть в браузере';
+}
+else {
+    TGSTR = 'Open in Telegram';
+    WEBSTR = 'Open in browser';
+}
 
 var url = window.location.hash.slice(1).replace(/^https?:\/\//i, '');
 var tgopen = document.getElementById('tgopen');
@@ -8,8 +16,9 @@ var webopen = document.getElementById('webopen');
 var target = document.getElementById('target');
 
 if (url) {
+    document.getElementById('buttons').style.display = 'inline-block';
 
-    document.getElementById('label').textContent = 'Open in Telegram';
+    document.getElementById('tglabel').textContent = TGSTR;
 
     tgopen.href = window.location.hash.slice(1);
 
@@ -37,20 +46,21 @@ if (url) {
             str = 'tg://resolve?domain=' + path[1] + tgopen.search.replace('?start=', '&start=');
             if (path[2]) {
                 str += '&post=' + path[2];
-            };
-            ischannel = true;
+            }
+            else
+                ischannel = true;
     }
 
     tgopen.href = str || '#';
 
     if (ischannel) {
         webopen.href = "https://tfeed.me/" + path[1];
-        document.getElementById('label3').textContent = 'Open in browser';
+        document.getElementById('weblabel').textContent = WEBSTR;
         webopen.style.display = 'inline-block';
     }
 
     target.href = window.location.hash.slice(1);
-    document.getElementById('label2').textContent = url;
+    document.getElementById('tlabel').textContent = url;
     target.style.display = 'inline-block';
 
     if (str) {
@@ -58,11 +68,9 @@ if (url) {
     }
 }
 else {
-    if (lang == "ru-RU")
-        var istr = "Как использовать";
-    else
-        istr = 'How to use';
-    document.getElementById('label').textContent = istr;
-    tgopen.href = 'https://github.com/roslovets/tg/blob/master/README.md';
+    var loc = window.location.toString();
+    if (loc[loc.length - 1] !== '#')
+        window.location.href = loc + '#';
+    document.getElementById('instr').style.display = 'inline-block';
 }
 tgopen.style.display = 'inline-block';
